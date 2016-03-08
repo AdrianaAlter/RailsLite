43cb86bd69be3c19bc225408dc945efb45bd48d7
Adriana <adriana.alter@gmail.com>
2016-03-08 17:49:03 -0500

    Build Rails Lite

---
+++ bin/p01_basic_server.rb
@@ -0,0 +1,23 @@
+require 'rack'
+require 'byebug'
+
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+
+  res['Content-Type'] = 'text/html'
+  #debugger
+  if req.path
+    res.write("#{req.path}")
+  else
+    res.write("Hello world!")
+  end
+  res.finish
+end
+
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
